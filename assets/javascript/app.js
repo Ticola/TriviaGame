eb197753424da07461fd4661b9b50676c5726b5f
Edgar Reynaga <edgar.reynaga@loop.colum.edu>
2018-10-06 00:09:05 -0500

    Final README and correct comments on JS by Edgar Reynaga

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 // Initial function that creates the START BUTTON and INTIAL SCREEN PAGE
 $(document).ready(function () {
 
+    // Create a function that is the intial screen and displays a button to start the quiz
     function initialScreen() {
         startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Press to Start Quiz</a></p>";
         $(".mainArea").html(startScreen);
@@ -34,6 +35,7 @@ $(document).ready(function () {
 
 });
 
+// Create a function that diaplays the question/answer choices/timer for each question
 function generateLossDueToTimeOut() {
     unansweredTally++;
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/correct.png'>";
@@ -41,6 +43,7 @@ function generateLossDueToTimeOut() {
     setTimeout(wait, 4000);
 }
 
+// Create a function assigned to keeping a record of how many questions are answered correctly and continue the timer and the game
 function generateWin() {
     correctTally++;
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
@@ -48,6 +51,7 @@ function generateWin() {
     setTimeout(wait, 4000);
 }
 
+// Create a function assigned to keeping a record of how many questions are answered incorrectly and continue the timer and the game
 function generateLoss() {
     incorrectTally++;
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/Wrong.png'>";;
@@ -55,11 +59,13 @@ function generateLoss() {
     setTimeout(wait, 4000);
 }
 
+// Create a function assigned to designating the answer letter to that of the questions array
 function generateHTML() {
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. " + answerArray[questionCounter][1] + "</p><p class='answer'>C. " + answerArray[questionCounter][2] + "</p><p class='answer'>D. " + answerArray[questionCounter][3] + "</p>";
     $(".mainArea").html(gameHTML);
 }
 
+//Create a function that will continue to cycle throughout the (7) Questions and display the (30 sec) timer and question accordingly
 function wait() {
     if (questionCounter < 7) {
         questionCounter++;
@@ -72,6 +78,7 @@ function wait() {
     }
 }
 
+// Create a function that controls the (30 sec) timer for each question and redirect to next question or generate loss due to timeout of game
 function timerWrapper() {
     theClock = setInterval(thirtySeconds, 1000);
     function thirtySeconds() {
@@ -86,11 +93,13 @@ function timerWrapper() {
     }
 }
 
+// Create a function that displays all the players performance and scores (Win, Loss, Questions answered)
 function finalScreen() {
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
     $(".mainArea").html(gameHTML);
 }
 
+// Create a function that will reset the game to its initial settings of 0 and allow the player to play again without refreshing the page
 function resetGame() {
     questionCounter = 0;
     correctTally = 0;
@@ -101,6 +110,7 @@ function resetGame() {
     timerWrapper();
 }
 
+// Create variables for all parameters of the game
 var startScreen;
 var gameHTML;
 var counter = 30;
